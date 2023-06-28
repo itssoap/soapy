df3b05dbb812c00d334f785a6fe2646620dfd677
itssoap <soaibkhan38@gmail.com>
2023-06-28 19:57:55 +0000

    Fix Pylance issues

--- soapy/utils/coloring.py
+++ soapy/utils/coloring.py
@@ -1,8 +1,9 @@
 from pygments import highlight, util
 from pygments.lexers import get_lexer_by_name, guess_lexer
 from pygments.formatters import HtmlFormatter
+from typing import Literal
 
-def colors(text: str) -> str:
+def colors(text: bytes | Literal['']) -> str:
     lexer = guess_lexer(text)  
     formatter = HtmlFormatter(linenos=True, style='github-dark')
     return highlight(text, lexer, formatter)
