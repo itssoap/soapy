99836f8d9f86d01dbe127a925f626c12ebfd2540
itssoap <soaibkhan38@gmail.com>
2023-07-18 14:08:31 +0000

    Fix markdown2 linking

--- soapy/utils/__init__.py
+++ soapy/utils/__init__.py
@@ -1,7 +1,7 @@
 from .info_logger import StubbedGunicornLogger, InterceptHandler, StandaloneApplication
 from .hashing import hashfunc
 from .coloring import colors
-from markdown2 import markdown
+from .markdown2 import markdown
 
 __all__ = [
     "StubbedGunicornLogger",
